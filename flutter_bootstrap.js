58f9c70d76e4b9a4f950f3e6392e9455407177a5
berviantoleo <berviantoleo@users.noreply.github.com>
2025-01-01 23:59:06 +0000

    deploy: d2f1f6864599681734997f33813dfcffa45473c9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"d7be573a5ee5e1ff7b5cf003185a06d6faf87f46","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"d9b7e5664604a55a3e900b0c619ee319065abb0e","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1300528979"
+    serviceWorkerVersion: "599493046"
   }
 });
